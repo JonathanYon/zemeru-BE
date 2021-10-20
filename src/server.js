2c9c2d25d933aa8fee1c2ef06b21935eb99dccf1
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-20 20:37:20 +0200

    server up and uning

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
-console.log("hello");
+import express from "express";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
+import mongoose from "mongoose";
+
+const server = express();
+const port = process.env.PORT || 3003;
+
+server.use(cors());
+server.use(express.json());
+
+mongoose.connect(process.env.MONGOS_CON);
+mongoose.connection.on(`connected`, () => {
+  // the string "connected" 👆☝ has to be "connected" nothing more nothing less
+  console.log(`🎁 mongo connected Successfully!!`);
+  server.listen(port, () => {
+    console.table(listEndpoints(server));
+    console.log(`server running on: ${port}`);
+  });
+});
+
+mongoose.connection.on(`error`, (err) => {
+  console.log(`Mongo Error: ${err}`);
+});
