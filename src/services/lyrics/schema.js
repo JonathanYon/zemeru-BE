5291bd97d41c2dba6d825b71eeac91ed4b4a73f4
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-21 14:20:23 +0200

    role and edited lyrics

--- src/services/lyrics/schema.js
+++ src/services/lyrics/schema.js
@@ -6,11 +6,24 @@ const lyricsSchema = new Schema(
   {
     artist: { type: String, required: true },
     title: { type: String, required: true },
-    lyrics: { type: String, required: true },
+    officialLyric: { type: String, required: true },
+    editedLyrics: [{ type: String }],
     releaseDate: { type: Date },
+    role: {
+      type: String,
+      required: true,
+      enum: ["User", "Editor"],
+      default: "User",
+    },
     youtubeLink: { type: String },
     coverImage: { type: String },
     mezmurType: { type: String, required: true },
+    comments: [
+      {
+        comment: String, //embedded comments inside the post for the comment
+      },
+    ],
+    likes: [{ type: Schema.Types.ObjectId, required: true, ref: "User" }],
   },
   { timestamps: true }
 );
