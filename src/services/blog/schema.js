752e8e24936ce4c494500b8f1a27fbab6d0e7df7
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-21 15:17:41 +0200

    blog schema

---
+++ src/services/blog/schema.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const blogSchema = new Schema(
+  {
+    category: { type: String, required: true },
+    title: { type: String, required: true },
+    cover: { type: String, required: true },
+    authors: [{ type: Schema.Types.ObjectId, required: true, ref: "Author" }],
+    content: { type: String, required: true },
+    comments: [
+      {
+        comment: String, //embedded comments inside the post for the comment
+      },
+    ],
+    likes: [{ type: Schema.Types.ObjectId, required: true, ref: "User" }],
+  },
+  { timestamps: true }
+);
+
+export default model(`Blog`, blogSchema);
