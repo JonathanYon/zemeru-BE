2a8a5add10e2766e0b641230aeb52c9134740a62
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-08 19:00:50 +0100

    comments of a user in blogs

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -132,6 +132,35 @@ usersRouter.get(
     }
   }
 );
+//comments of a user in blog
+usersRouter.get(
+  "/blogs/comments/:userId",
+  jwtAuthMiddleware,
+  async (req, res, next) => {
+    try {
+      console.log("am in try---");
+      const commentsLyr = await blogModel.find({
+        "comments.userId": req.params.userId,
+      });
+      if (commentsLyr) {
+        console.log("am in if---");
+        let idAndComments = commentsLyr.map((el) => {
+          return {
+            id: el._id,
+            comments: el.comments.map((el) => el.comment),
+          };
+        });
+        res.send(idAndComments);
+      } else {
+        res.send("no comments");
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+);
+
 // change me
 usersRouter.put("/me", jwtAuthMiddleware, async (req, res, next) => {
   try {
