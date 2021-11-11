4a3487c473ac37a9cd431dc79e16b182f87f341f
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-11 18:49:53 +0100

    message exchange now is robust

--- src/services/messages/index.js
+++ src/services/messages/index.js
@@ -8,13 +8,40 @@ const messagesRouter = Router();
 //post a lyrics
 messagesRouter.post("/:id", jwtAuthMiddleware, async (req, res, next) => {
   try {
-    const lyric = await messageModel({
-      ...req.body,
+    const chat = {
       from: req.user._id,
       to: req.params.id,
-    }).save();
+    };
 
-    res.status(201).send(lyric._id);
+    const message = await messageModel.findOne(chat);
+    // console.log("message", message);
+    if (message) {
+      const addMessage = await messageModel.findOneAndUpdate(
+        chat,
+        {
+          $push: {
+            messages: { ...req.body, from: req.user._id, to: req.params.id },
+          },
+        },
+        { new: true }
+      );
+      //   console.log("addmessage", addMessage);
+      res.status(201).send(addMessage);
+    } else {
+      const newMessage = await messageModel({
+        from: req.user._id,
+        to: req.params.id,
+        messages: [
+          {
+            message: req.body.message,
+            from: req.user._id,
+            to: req.params.id,
+          },
+        ],
+      }).save();
+
+      res.status(201).send(newMessage);
+    }
   } catch (error) {
     console.log(error);
     next(error);
