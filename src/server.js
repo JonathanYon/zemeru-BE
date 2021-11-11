78fe33e3a51f9e7b8914d24c7e68ceae0edfd378
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-11-11 13:31:07 +0100

    Pusher instance

--- src/server.js
+++ src/server.js
@@ -1,13 +1,23 @@
 import express from "express";
 import cors from "cors";
+import Pusher from "pusher";
 import listEndpoints from "express-list-endpoints";
 import mongoose from "mongoose";
 import lyricsRouter from "./services/lyrics/index.js";
 import usersRouter from "./services/users/index.js";
 import blogsRouter from "./services/blog/index.js";
+import messagesRouter from "./services/messages/index.js";
 
 const server = express();
 const port = process.env.PORT || 3003;
+
+const pusher = new Pusher({
+  appId: "1295753",
+  key: "0fb50def5b9d8d12554b",
+  secret: "5dd0b0329f2879beccf9",
+  cluster: "eu",
+  useTLS: true,
+});
 
 server.use(cors());
 server.use(express.json());
@@ -15,6 +25,7 @@ server.use(express.json());
 server.use("/lyrics", lyricsRouter);
 server.use("/users", usersRouter);
 server.use("/blogs", blogsRouter);
+server.use("/messages", messagesRouter);
 
 mongoose.connect(process.env.MONGOS_CON);
 mongoose.connection.on(`connected`, () => {
@@ -23,6 +34,15 @@ mongoose.connection.on(`connected`, () => {
   server.listen(port, () => {
     console.table(listEndpoints(server));
     console.log(`server running on: ${port}`);
+  });
+});
+
+mongoose.connection.once("open", () => {
+  console.log("another one");
+  const msgCollection = mongoose.connection.collection("Messages");
+  const changeStream = msgCollection.watch();
+  changeStream.on("change", (change) => {
+    console.log(change);
   });
 });
 
